9562b7ff82bdea507dcabbf69c5440e9f97575e7
Gabriel Fernando <gabr.fern99@gmail.com>
2020-11-07 14:27:00 -0300

    Change icon layout

--- config.h
+++ config.h
@@ -69,7 +69,7 @@ static Sp scratchpads[] = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
