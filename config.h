f2f0a2f34bdda3154c457aa1ebb5ca89582889d6
Gabriel Fernando <gabr.fern99@gmail.com>
2021-01-15 11:57:04 -0300

    Set smartgaps to true and define it's values

--- config.h
+++ config.h
@@ -6,13 +6,13 @@ static unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static unsigned int gappih    = 0;       /* horiz inner gap between windows */
- const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
+static const int showsystray = 1;     /* 0 means no systray */
+static unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "Fira Code:size=9:antialias=true:autohint=true", "JoyPixels:pixelsize=12:antialias=true:autohint=true", "FontAwesome:pixelsize=15:antialias=true:autohint=true",};
@@ -85,6 +85,7 @@ static const Rule rules[] = {
 	{ "St",       NULL,       NULL,       0,            0,           1,         0,        -1 },
 	{ NULL,       "St",       NULL,       0,            0,           1,         0,        -1 },
 	{ "qutebrowser", NULL, NULL, 1 << 1, 0, 0, -1 },
+	{ "Xterm", NULL, NULL, 1 << 7, 1, 1, -1 },
 	{ "Virt-manager", NULL, NULL, 0, 1, 0, 0, -1 },
 	{ "Steam", NULL, NULL, 1 << 3, 0, 0, 0, -1 },
 	{ "mpv", NULL, NULL, 0, 1, 0, 0, -1 },
