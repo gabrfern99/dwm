93ad9cde3ac6132041357eaa26751fc62c78b5b0
Gabriel Fernando <gabr.fern99@gmail.com>
2021-02-22 13:47:47 -0300

    Change default tag icons

--- config.h
+++ config.h
@@ -99,7 +99,7 @@ static Sp scratchpads[] = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
